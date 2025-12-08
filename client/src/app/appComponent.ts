2b22c31353b770f9d3bfe6f0da517f4eac5731a7
Aaron <a.a.r.g@outlook.com>
2025-12-08 03:37:52 -0600

    Angualr Started

---
+++ client/src/app/appComponent.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './appComponent.html',
+  styleUrl: './appComponent.scss'
+})
+export class AppComponent {
+  title = 'client';
+}
